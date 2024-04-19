7fb33cca78c51bcece0826f36f6e6dbd99be7c9a
Clara Ruiz-Santacruz Jiménez <clara.ruizsantacruz@gmail.com>
2024-04-19 21:35:38 +0200

    00 - JavaScript

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Clara-RuizSantacruz.js
@@ -0,0 +1,27 @@
+// Comentario de una sola línea: https://www.javascript.com/
+
+/*
+Comentario
+de varias líneas
+*/
+
+var nombre = "Clara";
+let apellido = "Ruiz";
+const PI = 3.14;
+
+let númeroEntero = 10;
+let númeroFlotante = 2.5;
+let númeroReal = -10;
+let string = "Hola";
+let char = 'a';
+let verdadero = true;
+let falso = false;
+let object = {
+    nombre: "Clara",
+    apellido: "Ruiz"
+}
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log("¡Hola, JavaScript!");
+
+
