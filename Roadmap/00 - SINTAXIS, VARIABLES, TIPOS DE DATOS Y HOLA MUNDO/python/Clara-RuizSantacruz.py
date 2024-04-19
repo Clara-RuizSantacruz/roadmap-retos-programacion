b633058bec5a0045f8b41e3674b040ef5d7ab103
Clara Ruiz-Santacruz Jiménez <clara.ruizsantacruz@gmail.com>
2024-04-19 19:56:02 +0200

    00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Clara-RuizSantacruz.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+#Esto es un comentario de una línea con el enlace a la web oficial dePython https://www.python.org/
+"""
+Esto es un comentario
+de varias lineas
+"""
+horasdeldía = 24
+PI = 3.1416
+
+int = 27
+float = 9.9
+str = 'Hola mundo'
+bool = False
+bool = True
+StrDeVariasLíneas = """
+    Esto es un string
+    de varias lineas"""
+StrChar = 'a'
+
+print("¡Hola, Python!")
+
+
